ad193b24cbb2e683dc1b54b7a616f334e235e851
viher3 <viher3@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-03 09:35:26 +0000

    - Update to version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/easy-eu-cookie-law/tags/1.2.1@1037345 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lang/en.php
@@ -0,0 +1,39 @@
+<?php
+
+    $lang['cookie_title'] = "Use of cookies";
+    $lang['cookie_text'] = "This website uses cookies for having the best user experience. If you continue browsing you are giving your consent to the cookies acceptance and our ";
+    $lang['polit_cook'] = "cookies policy";
+    $lang['accept'] = "Accept";
+
+    //admin
+    $lang['admin_title'] = "Cookie Law Settings";
+    $lang['save_ok'] = "Data saved correctly.";
+    $lang['save_err'] = "Error saving data, try it again.";
+    $lang['language'] = "Language:";
+    $lang['english'] = "English";
+    $lang['spanish'] = "Spanish";
+    $lang['alert_color'] = "Alert color:";    
+    
+    $lang['grey'] = "Grey"; //Grey
+    $lang['grey2'] = "Grey 2"; //Grey 2
+    $lang['blue'] = "Blue";
+    $lang['river_blue'] = "River blue";
+    $lang['night_blue'] = "Night blue";
+    $lang['green'] = "Green";
+    $lang['dark_green'] = "Dark green";
+    $lang['turquoise_green'] = "Turquoise green";
+    $lang['emerald_green'] = "Emerald green";
+    $lang['pink'] = "Pink";
+    $lang['dark_pink'] = "Dark pink";
+    $lang['asphalt'] = "Asphalt";
+    $lang['light_yellow'] = "Light yellow";
+    $lang['light_orange'] = "Light orange";
+    $lang['carrot_orange'] = "Carrot orange";
+    $lang['dark_orange'] = "Dark orange";
+    $lang['light_red'] = "Light red";
+    $lang['dark_red'] = "Dark red";
+    
+    $lang['pol_url'] = "Cookies policy url:";
+    $lang['save_changes'] = "Save changes";
+    
+?>
